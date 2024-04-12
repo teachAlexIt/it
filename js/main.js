9fd1f909d19efde371cac0ccd48568a5b3add7f3
teachAlexIt <alekseiteacherit@gmail.com>
2024-04-12 20:01:05 +0300

    12/04/24 add cours deck

--- js/main.js
+++ js/main.js
@@ -5,34 +5,38 @@ const navContact = document.querySelector('.nav__contact');
 const navLogo = document.querySelector('.nav__logo');
 const linkAll = document.querySelectorAll('.nav__links a');
 const navContactLinks = document.querySelectorAll('.nav__contact a');
-hamburger.onclick = function() {
-    hamburger.classList.toggle("hamburger_active"); 
-    navLink.classList.toggle("nav__links_active");
-    navContact.classList.toggle("nav__contact_active");
-    navLogo.classList.toggle("nav__logo_active");
+hamburger.onclick = function () {
+  hamburger.classList.toggle("hamburger_active");
+  navLink.classList.toggle("nav__links_active");
+  navContact.classList.toggle("nav__contact_active");
+  navLogo.classList.toggle("nav__logo_active");
 }
-navLogo.onclick = function() {
-    navLogo.classList.remove("nav__logo_active");
+navLogo.onclick = function () {
+  navLogo.classList.remove("nav__logo_active");
+  hamburger.classList.remove("hamburger_active");
+  navContact.classList.remove("nav__contact_active");
+  navLink.classList.remove("nav__links_active");
+}
+for (let i = 0; i < linkAll.length; i++) {
+  linkAll[i].onclick = function () {
     hamburger.classList.remove("hamburger_active");
-    navContact.classList.remove("nav__contact_active");
     navLink.classList.remove("nav__links_active");
+    navContact.classList.remove("nav__contact_active");
+    navLogo.classList.remove("nav__logo_active");
+  }
 }
-for(let i = 0; i<linkAll.length;i++){
-    linkAll[i].onclick = function() {
-        hamburger.classList.remove("hamburger_active"); 
-        navLink.classList.remove("nav__links_active");
-        navContact.classList.remove("nav__contact_active");
-        navLogo.classList.remove("nav__logo_active");
-    }
+for (let i = 0; i < navContactLinks.length; i++) {
+  navContactLinks[i].onclick = function () {
+    hamburger.classList.remove("hamburger_active");
+    navLink.classList.remove("nav__links_active");
+    navContact.classList.remove("nav__contact_active");
+    navLogo.classList.remove("nav__logo_active");
+  }
 }
-for(let i = 0; i<navContactLinks.length;i++){
-    navContactLinks[i].onclick = function() {
-        hamburger.classList.remove("hamburger_active"); 
-        navLink.classList.remove("nav__links_active");
-        navContact.classList.remove("nav__contact_active");
-        navLogo.classList.remove("nav__logo_active");
-    }
-}
+
+
+
+
 
 
 
@@ -40,45 +44,66 @@ const body = document.querySelector('body');
 // ProtectionText
 const mainWrapper = document.querySelector('.main-wrapper');
 const protectionSpan = document.querySelector('.protection');
-const modalBlocks = document.querySelectorAll('.modal-block');
+const modalBlock = document.querySelector('.modal-block');
+import courseDescriptionStore from './courseDescriptStorage.js';
+console.log(courseDescriptionStore);
 protectionSpan.onclick = function () {
+  modalBlock.innerHTML = courseDescriptionStore[courseDescriptionStore.length - 1];
+  modalBlock.scrollTop = 0;
   mainWrapper.classList.add('_show');
   body.classList.add('_fixid');
-  modalBlocks[0].classList.add('_show');
+  modalBlock.classList.add('_show');
 }
 mainWrapper.onclick = function (e) {
   if (e.target.classList.contains('main-wrapper')) {
     mainWrapper.classList.remove('_show');
     body.classList.remove('_fixid');
-    modalBlocks[0].classList.remove('_show');
+    modalBlock.classList.remove('_show');      
   }
 }
 
+// 
 
-// choiceCourses
 
-let choiceCourses = '';
-
-const coursesLinkAll = document.querySelectorAll('.courses button');
-for (let i = 0; i < coursesLinkAll.length; i++) {
-  coursesLinkAll[i].onclick = function () {
-    coursesLinkAll[i].classList.toggle('_choice');
-    if (coursesLinkAll[i].classList.contains('_choice')) {
-      coursesLinkAll[i].innerHTML = '<i class="fa-regular fa-circle-check"></i>Выбран<i class="fa-regular fa-circle-check"></i>';
-    } else {
-      coursesLinkAll[i].innerHTML = 'Выбрать';
+const courseDescriptionButton = document.querySelectorAll('.course-card_more');
+let visitCourses = '';
+for (let i = 0; i < courseDescriptionButton.length; i++) {
+  courseDescriptionButton[i].onclick = function () {
+    courseDescriptionButton[i].classList.add('_visit');
+    const visitCoursesButton = document.querySelectorAll('.courses ._visit');
+    visitCourses = '';
+    for (let i = 0; i < visitCoursesButton.length; i++) {
+      visitCourses = visitCourses + (visitCoursesButton[i].parentNode.parentNode.querySelector('h3').innerText) + ' / ';
     }
-    const courseschoiceButton = document.querySelectorAll('.courses ._choice');
-    choiceCourses = '';
-    for (let i = 0; i < courseschoiceButton.length; i++) {
-      choiceCourses = choiceCourses + (courseschoiceButton[i].parentNode.parentNode.querySelector('h3').innerText) + ' / ';
-    }
-    console.log(choiceCourses);
+    modalBlock.scrollTop = 0;
+    mainWrapper.classList.add('_show');
+    body.classList.add('_fixid');
+    modalBlock.innerHTML = courseDescriptionStore[i];
+    modalBlock.classList.add('_show');
   }
+
 }
+
+
+
+
+
 
 // formSubmit
 const formContact = document.querySelector('.contact form');
+let chatToken = '';
+document.addEventListener("DOMContentLoaded", function () {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', '/js/chatToken.php', true);
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      var response = xhr.responseText;
+      var a = response.trim(); // Убираем возможные лишние пробелы и переносы строки
+      chatToken = a;
+    }
+  };
+  xhr.send();
+});
 
 formContact.onsubmit = function () {
   const formSubmitBlock = formContact.querySelector('.form-submit');
@@ -89,12 +114,13 @@ formContact.onsubmit = function () {
     formSubmitBlock.classList.remove('_show');
   }, 3000);
 
-  const chatToken = '6787781737:AAGpLJ84BHUon0i6p9mxa3EecA-GPrDTgL4';
+
   const chatId = '396606827';
 
+  console.log(chatToken);
   const Name = formContact.querySelector('#name').value;
   const Tel = formContact.querySelector('#tel').value;
-  const messageText = `💥🎉Новая заявка с сайта\n🔘Имя - ${Name}\n🔘Почта - ${Tel}\n🔘Курс - ${choiceCourses}`;
+  const messageText = `💥🎉Новая заявка с сайта\n🔘Имя - ${Name}\n🔘Почта - ${Tel}\n🔘Курсы - ${visitCourses}`;
 
   const url = `https://api.telegram.org/bot${chatToken}/sendMessage`;
   const params = {
@@ -111,3 +137,8 @@ formContact.onsubmit = function () {
   return false
 
 }
+
+
+
+
+
